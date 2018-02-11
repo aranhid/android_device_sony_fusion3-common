192599ea75ec5f9cccc6d0c16faeef0536872480
CyberWalkMaN <firefox883@gmail.com>
2018-02-11 17:20:09 +0500

    fusion3: Move proprietaty files to vendor partition[TEST]
    Change-Id: I24282e7750ab705690e40e1d06c1828e8f85c561

--- board/shim.mk
+++ board/shim.mk
@@ -1,5 +1,5 @@
 # SHIM
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libcald_pal.so|libshim_cald.so \
-    /system/lib/hw/camera.vendor.qcom.so|libshim_camera.so \
-    /system/lib/libMPU3050.so|libshim_MPU3050.so
+    /system/vendor/lib/libcald_pal.so|libshim_cald.so \
+    /system/vendor/lib/hw/camera.vendor.qcom.so|libshim_camera.so \
+    /system/vendor/lib/libMPU3050.so|libshim_MPU3050.so
--- rootdir/etc/init.qcom.bt.sh
+++ rootdir/etc/init.qcom.bt.sh
@@ -89,9 +89,9 @@ esac
 
 if [ -z "$BDADDR" ]
 then
-  /system/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS -vv
+  /vendor/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS -vv
 else
-  /system/bin/hci_qcomm_init -b $BDADDR -e $PWR_CLASS $LE_PWR_CLASS -vv
+  /vendor/bin/hci_qcomm_init -b $BDADDR -e $PWR_CLASS $LE_PWR_CLASS -vv
 fi
 
 case $? in
